08af10aff481a054033231d19b2c916b9bc053ab
Asia Gagnon <asiajoyg@gmail.com>
2019-06-11 15:18:11 -0400

    friend and user models

---
+++ server/db/models/friend.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize')
+const db = require('..')
+
+const Friend = db.define('friend', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  likes: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  activities: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT(2),
+    allowNull: false
+  },
+  instock: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    default: true
+  },
+  image: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+module.exports = Friend
