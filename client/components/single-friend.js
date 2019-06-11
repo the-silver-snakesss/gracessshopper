428fe9fd4b024f144ed663eb6374f8074e9ba1b3
Allison Geismar <ageismar213@gmail.com>
2019-06-11 18:01:49 -0400

    single friend component rendering info

--- client/components/single-friend.js
+++ client/components/single-friend.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const SingleFriend = props => {
-  return <div>{/*should display img, name, and price*/}</div>
+  const {name, image, price} = props
+  return (
+    <div>
+      <img src={image} />
+      <h3>{name}</h3>
+      <h4>{price}</h4>
+    </div>
+  )
 }
 export default SingleFriend
