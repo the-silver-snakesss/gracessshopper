39db5826a13d2f8768cf7bd6a84db7c458478e1f
blydiet <lydietoussaint123@gmail.com>
2019-06-11 15:58:39 -0400

    made react components

---
+++ client/components/checkout-form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class CheckoutForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input placeholder="place in a vlaue" type="text" />
+        </form>
+      </div>
+    )
+  }
+}
+export default CheckoutForm
--- client/components/single-friend.js
+++ client/components/single-friend.js
@@ -3,3 +3,4 @@ import React from 'react'
 const SingleFriend = props => {
   return <div>{/*should display img, name, and price*/}</div>
 }
+export default SingleFriend
