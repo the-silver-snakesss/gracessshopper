c2446c623e3e3580149e57d6ef6e6f25c59aff02
Natasha Kelly <kellynatashav@yahoo.com>
2019-06-11 16:20:05 -0400

    updated ORDER table --removed username

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,11 +2,8 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  username: {
-    type: Sequelize.STRING
-  },
   address: {
-    type: Sequelize.STRING
+    type: Sequelize.TEXT
   }
 })
 
