239c93c0ab5e128cc909cc0561fead2ab5699c24
Asia Gagnon <asiajoyg@gmail.com>
2019-06-12 14:31:00 -0400

    fix: (db/models) changed through table to order_friends and added qty col. Changed associations in index to reflect changes

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  address: {
-    type: Sequelize.TEXT
+  Status: {
+    type: Sequelize.ENUM('pending', 'complete')
   }
 })
 
