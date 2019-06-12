729e9afea5951700573e1069e1950c0a8c6a8629
Asia Gagnon <asiajoyg@gmail.com>
2019-06-12 11:25:40 -0400

    more seeded friends

--- script/seed.js
+++ script/seed.js
@@ -51,55 +51,50 @@ const seed = async () => {
       description: 'First rule of Fight club...',
       likes: 'anarchy',
       activities: 'Fighting',
-      price: 2.0,
+      price: 19.99,
       instock: true,
       image:
-        'https://vignette.wikia.nocookie.net/villains/images/7/7b/Tylerbetterpicture.jpg/revision/latest/scale-to-width-down/620?cb=20170410170911',
-      userId: user1.id
+        'https://vignette.wikia.nocookie.net/villains/images/7/7b/Tylerbetterpicture.jpg/revision/latest/scale-to-width-down/620?cb=20170410170911'
     })
 
     const friend2 = await Friend.create({
       name: 'Hobbes',
       description: 'Sardonic stuffed tiger',
-      likes: 'going for walks',
+      likes: 'tuna fish',
       activities: 'Leisure',
-      price: 50.5,
+      price: 19.85,
       instock: true,
       image:
-        'https://vignette.wikia.nocookie.net/dccomicsfannon/images/1/1f/Hobbes.jpg/revision/latest?cb=20171221225748',
-      userId: user4.id
+        'https://vignette.wikia.nocookie.net/dccomicsfannon/images/1/1f/Hobbes.jpg/revision/latest?cb=20171221225748'
     })
     const friend3 = await Friend.create({
       name: 'Frank',
       description: 'I can be your guide',
       likes: 'science',
       activities: 'talking about space and time continuum ',
-      price: 50.0,
+      price: 20.01,
       instock: true,
-      image: 'https://cdn-images-1.medium.com/max/1600/0*wou131aVtMKPjkyh.jpg',
-      userId: user2.id
+      image: 'https://cdn-images-1.medium.com/max/1600/0*wou131aVtMKPjkyh.jpg'
     })
     const friend4 = await Friend.create({
       name: 'Lloyd',
       description: 'Bartender',
       likes: 'spending time in the Overlook Hotel',
       activities: 'drinking',
-      price: 40.2,
+      price: 19.77,
       instock: false,
       image:
-        'https://preview.redd.it/kqt98zhromn11.jpg?width=960&crop=smart&auto=webp&s=94b1730968b4d64af9ec0f41eee4bd0615a343d3',
-      userId: user3.id
+        'https://preview.redd.it/kqt98zhromn11.jpg?width=960&crop=smart&auto=webp&s=94b1730968b4d64af9ec0f41eee4bd0615a343d3'
     })
     const friend5 = await Friend.create({
       name: 'Snuffleupagus',
       description: 'big bird best friend',
       likes: 'cuddling',
       activities: 'being a good best friend',
-      price: 150.3,
+      price: 19.69,
       instock: true,
       image:
-        'https://vignette.wikia.nocookie.net/muppet/images/3/3a/Alices.jpg/revision/latest?cb=20161119035937',
-      userId: user5.id
+        'https://vignette.wikia.nocookie.net/muppet/images/3/3a/Alices.jpg/revision/latest?cb=20161119035937'
     })
     const friend6 = await Friend.create({
       name: 'George Glass',
@@ -112,7 +107,47 @@ const seed = async () => {
       image:
         'https://vignette.wikia.nocookie.net/thebradybunch/images/4/41/George_Glass.png/revision/latest?cb=20180202032210'
     })
-
+    const friend7 = await Friend.create({
+      name: 'Bunbury',
+      description:
+        'An English gentleman from the Victorian era with remarkably bad health',
+      likes:
+        'The English countryside and getting you out of unpleasant obligations.',
+      activities: 'Staying home sick with you',
+      price: 18.95,
+      instock: true,
+      image:
+        '“https://i.kinja-img.com/gawker-media/image/upload/s--IARvqZA1--/c_scale,f_auto,fl_progressive,q_80,w_800/xwavtb38s3qkxds4cnop.jpg'
+    })
+    const friend8 = await Friend.create({
+      name: 'Carol',
+      description:
+        'A wild thing from an island far far away, where wild things roam free!',
+      likes: 'Sleeping in a big pile and building models',
+      activities: 'Dirt fights and island tours',
+      price: 19.63,
+      instock: true,
+      image:
+        'http://media.onsugar.com/files/ed2/192/1922283/42_2009/39f8db1ba3ef2957_where-the-wild-things-are.jpg'
+    })
+    const friend9 = await Friend.create({
+      name: 'Charles',
+      description: 'A rowdy pal who pushes you to open up socially',
+      likes: 'throwing your homework out the window and his niece Marcee',
+      activities: 'taking a break from work and partying',
+      price: 20.01,
+      instock: true,
+      image: 'https://www.aveleyman.com/Gallery/2017/B/37883-25696.jpg'
+    })
+    const friend10 = await Friend.create({
+      name: 'Bloo',
+      description: 'A blue friend with a big ego and even bigger heart',
+      likes: 'getting into trouble and bragging',
+      activities: 'paddle ball tournaments',
+      price: 20.04,
+      instock: true,
+      image: 'https://img.photobucket.com/albums/1003/vampwill/BlooKing.jpg'
+    })
     // ORDERS
 
     await Promise.all([
@@ -122,6 +157,10 @@ const seed = async () => {
       friend4,
       friend5,
       friend6,
+      friend7,
+      friend8,
+      friend9,
+      friend10,
       user1,
       user2,
       user3,
@@ -138,6 +177,10 @@ const seed = async () => {
         friend4,
         friend5,
         friend6,
+        friend7,
+        friend8,
+        friend9,
+        friend10,
         user1,
         user2,
         user3,
