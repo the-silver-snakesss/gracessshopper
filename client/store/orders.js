84aae2a34c2d2c3d1daae9f04ec82aaa5ec566ae
Asia Gagnon <asiajoyg@gmail.com>
2019-06-13 18:38:47 -0400

    fix: (store/orders) cleaned up code on viewing empty cart fix

--- client/store/orders.js
+++ client/store/orders.js
@@ -41,7 +41,6 @@ export const getOrdersThunk = userId => async dispatch => {
 export const getCartThunk = userId => async dispatch => {
   try {
     const {data} = await axios.get(`/api/orders/pending/${userId}`)
-    console.log('DATA', data)
     if (data) {
       dispatch(gotCart(data))
     } else {
