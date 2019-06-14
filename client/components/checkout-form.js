c2c776fda226c78226c0c671509906cc27508433
Asia Gagnon <asiajoyg@gmail.com>
2019-06-14 08:35:33 -0400

    feat: (models/orders, components/checkout-form) added address and name feilds to Orders model, added address and name input feilds to checkout form

--- client/components/checkout-form.js
+++ client/components/checkout-form.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
 class CheckoutForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address: ''
+    }
+  }
   render() {
     return (
       <div>
         <form>
-          <input placeholder="place in a vlaue" type="text" />
+          <label>First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={evt => this.setState({firstName: evt.target.value})}
+          />
+          <label>Last Name</label>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={evt => this.setState({lastName: evt.target.value})}
+          />
+          <label>Address</label>
+          <input
+            type="text"
+            name="address"
+            value={this.state.address}
+            onChange={evt => this.setState({address: evt.target.value})}
+          />
         </form>
       </div>
     )
